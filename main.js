44cc3560175eb2222a8d82502707bec7059afc7c
gamegrizzly_offical <161536721+gamegrizzly@users.noreply.github.com>
2024-03-10 21:20:06 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gamegrizzly</title>
+</head>
+<body>
+
+    <h1>Welcome to the gamegrizzly website!</h1>
+
+    <h2>Proxy</h2>
+    <iframe src="https://benjaminwhitemadethisone.petunia.ro/" width="100%" height="500"></iframe>
+
+    <h2>Embedded Games</h2>
+    <iframe src="https://1v1lol.com" width="100%" height="500"></iframe>
+
+</body>
+</html>
